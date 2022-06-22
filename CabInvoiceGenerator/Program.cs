5c8dc2eb5910b2ad2c22dfc16859256aa5a29ab9
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-22 16:43:02 +0530

    Added to main

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabInvoiceGenerator
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Cab Invoice Generator program");
+        }
+    }
+}
