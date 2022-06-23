89cbeae62eefd7a7f5975fdabe1210aeeae7b1ba
Vivek kumar <viveksinghssm496@gmail.com>
2022-06-23 15:04:47 +0530

    Added to CalculateFare

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -1,10 +1,14 @@
 ﻿namespace CabInvoiceGenerator
 {
-    internal class program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t\t\t\t\tWelcome to Cab Invoice Generator program");
+            Console.WriteLine("\t\t\t\t\tWecome to Cab Invoice Generator Program\n");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine("Total fare : " + fare);
         }
     }
 }
